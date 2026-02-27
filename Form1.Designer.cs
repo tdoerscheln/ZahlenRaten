02135f43ac30ce75b656b474c7abc046924cc408
Thomas Dörscheln <thomas.doerscheln@outlook.com>
2026-02-27 17:59:43 +0100

    Initial push

---
+++ Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace ZahlenRaten
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxEingabe = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.starten = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.labelErgebnis = new System.Windows.Forms.Label();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxEingabe
+            // 
+            this.textBoxEingabe.Location = new System.Drawing.Point(271, 110);
+            this.textBoxEingabe.Name = "textBoxEingabe";
+            this.textBoxEingabe.Size = new System.Drawing.Size(100, 22);
+            this.textBoxEingabe.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(238, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "zum starten zahl eingeben";
+            // 
+            // starten
+            // 
+            this.starten.Location = new System.Drawing.Point(404, 110);
+            this.starten.Name = "starten";
+            this.starten.Size = new System.Drawing.Size(75, 23);
+            this.starten.TabIndex = 2;
+            this.starten.Text = "starten";
+            this.starten.UseVisualStyleBackColor = true;
+            this.starten.Click += new System.EventHandler(this.starten_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(2, -1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(189, 105);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Einstellung";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.groupBox2.Location = new System.Drawing.Point(271, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(173, 19);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "ZAHLEN ERRATEN";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelErgebnis);
+            this.groupBox3.Location = new System.Drawing.Point(217, 210);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(262, 189);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Ergebnis";
+            // 
+            // labelErgebnis
+            // 
+            this.labelErgebnis.AutoSize = true;
+            this.labelErgebnis.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelErgebnis.Location = new System.Drawing.Point(83, 76);
+            this.labelErgebnis.Name = "labelErgebnis";
+            this.labelErgebnis.Size = new System.Drawing.Size(152, 39);
+            this.labelErgebnis.TabIndex = 0;
+            this.labelErgebnis.Text = "Ergebnis";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.starten);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxEingabe);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxEingabe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button starten;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label labelErgebnis;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZahlenRaten
+{
+    public partial class Form1 : Form
+    {
+        int zahl;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            setzeZahl();
+        }
+
+        private void setzeZahl()
+        {
+            zahl = (new Random()).Next(100);
+        }
+
+        private void starten_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBoxEingabe.Text, out int eingabeZahl))
+            {
+                MessageBox.Show("Das ist keine Zahl!");
+                textBoxEingabe.Text = "";
+                return;
+            }
+
+            if (eingabeZahl == zahl)
+            {
+                labelErgebnis.Text = "TREFFER!";
+            }
+            else if (eingabeZahl > zahl)
+            {
+                labelErgebnis.Text = "niedriger";
+            }
+            else
+            {
+                labelErgebnis.Text = "höher";
+            }
+        }
+    }
+}
